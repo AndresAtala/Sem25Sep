065895a0bf5194c192589386531cbea44bf58684
AndresAtala <132239849+AndresAtala@users.noreply.github.com>
2023-10-20 03:18:29 -0300

    Add files via upload

---
+++ src/main/java/gui/VentanaRegistroCarrera.java
@@ -0,0 +1,51 @@
+package gui;
+
+import javax.swing.*;
+
+public class VentanaRegistroCarrera {
+    public VentanaRegistroCarrera() {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Registro Carrera");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            JPanel panel = new JPanel();
+            panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+            JLabel nombreLabel = new JLabel("Nombre de la Carrera: ");
+            JTextField nombreField = new JTextField(20);
+
+            JLabel codigoLabel = new JLabel("Código de la Carrera: ");
+            JTextField codigoField = new JTextField(20);
+
+            JLabel semestresLabel = new JLabel("Cantidad de Semestres: ");
+            JTextField semestresField = new JTextField(20);
+
+            JButton registrarButton = new JButton("Registrar");
+            registrarButton.addActionListener(e -> {
+                String nombre = nombreField.getText();
+                String codigo = codigoField.getText();
+                int semestres = Integer.parseInt(semestresField.getText());
+                // Aquí puedes agregar la lógica para registrar la carrera
+            });
+
+            JButton volverButton = new JButton("Volver");
+            volverButton.addActionListener(e -> {
+                frame.dispose();
+                VentanaBienvenida.main(new String[]{});
+            });
+
+            panel.add(nombreLabel);
+            panel.add(nombreField);
+            panel.add(codigoLabel);
+            panel.add(codigoField);
+            panel.add(semestresLabel);
+            panel.add(semestresField);
+            panel.add(registrarButton);
+            panel.add(volverButton);
+
+            frame.getContentPane().add(panel);
+            frame.pack();
+            frame.setVisible(true);
+        });
+    }
+}
--- src/main/java/gui/VentanaRegistroEstudiante.java
+++ src/main/java/gui/VentanaRegistroEstudiante.java
@@ -1,58 +1,57 @@
 package gui;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class VentanaRegistroEstudiante {
     public VentanaRegistroEstudiante() {
-        JFrame frame = new JFrame("Registro Estudiante");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Registro Estudiante");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel panel = new JPanel();
+            JPanel panel = new JPanel();
+            panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
-        JLabel nombreLabel = new JLabel("Nombre: ");
-        JTextField nombreField = new JTextField(20);
+            JLabel nombreLabel = new JLabel("Nombre: ");
+            JTextField nombreField = new JTextField(20);
 
-        JLabel apellidoLabel = new JLabel("Apellido: ");
-        JTextField apellidoField = new JTextField(20);
+            JLabel apellidoLabel = new JLabel("Apellido: ");
+            JTextField apellidoField = new JTextField(20);
 
-        JLabel rutLabel = new JLabel("RUT: ");
-        JTextField rutField = new JTextField(20);
+            JLabel rutLabel = new JLabel("RUT: ");
+            JTextField rutField = new JTextField(20);
 
-        JLabel matriculaLabel = new JLabel("Número de Matrícula: ");
-        JTextField matriculaField = new JTextField(20);
+            JLabel matriculaLabel = new JLabel("Número de Matrícula: ");
+            JTextField matriculaField = new JTextField(20);
 
-        JButton registrarButton = new JButton("Registrar");
-        registrarButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Aquí puedes realizar la lógica para registrar al estudiante
+            JButton registrarButton = new JButton("Registrar");
+            registrarButton.addActionListener(e -> {
                 String nombre = nombreField.getText();
                 String apellido = apellidoField.getText();
                 String rut = rutField.getText();
                 int matricula = Integer.parseInt(matriculaField.getText());
+                // Aquí puedes agregar la lógica para registrar al estudiante
+            });
 
-                // Aquí podrías usar la clase Estudiante para crear una instancia y trabajar con ella
-                // Estudiante estudiante = new Estudiante(nombre, apellido, rut, matricula);
+            JButton volverButton = new JButton("Volver");
+            volverButton.addActionListener(e -> {
+                frame.dispose();
+                VentanaBienvenida.main(new String[]{});
+            });
 
-                // Lógica adicional según tus requisitos
-            }
+            panel.add(nombreLabel);
+            panel.add(nombreField);
+            panel.add(apellidoLabel);
+            panel.add(apellidoField);
+            panel.add(rutLabel);
+            panel.add(rutField);
+            panel.add(matriculaLabel);
+            panel.add(matriculaField);
+            panel.add(registrarButton);
+            panel.add(volverButton);
+
+            frame.getContentPane().add(panel);
+            frame.pack();
+            frame.setVisible(true);
         });
-
-        panel.add(nombreLabel);
-        panel.add(nombreField);
-        panel.add(apellidoLabel);
-        panel.add(apellidoField);
-        panel.add(rutLabel);
-        panel.add(rutField);
-        panel.add(matriculaLabel);
-        panel.add(matriculaField);
-        panel.add(registrarButton);
-
-        frame.getContentPane().add(panel);
-        frame.pack();
-        frame.setVisible(true);
     }
 }
-
